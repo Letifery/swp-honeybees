3a6ead2a1ac649b616eea54da1ba28994dceb141
Letifery <34723172+Letifery@users.noreply.github.com>
2022-07-05 17:50:35 +0200

    Update to v1.0.1
    - Renamed some variables for more consistency
    - You can now set the prefix of datalog file names for better differentiation between multiple models

--- ppevaluation.py
+++ ppevaluation.py
@@ -37,11 +37,13 @@ def aggregate_data(data, pickle_file):
 path_data = r"I:\tmp_swp\wdd_ground_truth"
 path_pickle = r"I:\tmp_swp\ground_truth_wdd_angles.pickle"
 path_testsuite = r"I:\tmp_swp\data\pptestsuite.json"
+#Sets the filename prefix for the logging tool
+model_name = "cnn3D-mv2"
 
 dl = DataLoader()
-data_logger = Logger("review.log")
+data_logger = Logger("review_%s.log" % model_name)
 #Below should be uncommented for the first iteration
-#data_logger.log_data([["ID", "CPU", "runtime", "loss", "cat_accuracy", "cat_entropy", "pp_layers", "confusion_matrix"]])
+data_logger.log_data([["ID", "CPU", "runtime", "loss", "cat_accuracy", "cat_entropy", "pp_layers", "confusion_matrix"]])
 gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.85)
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
 
@@ -53,7 +55,7 @@ X, Y, json_files, paths = aggregate_data(data, pickle_file)
 Y_classes, Y_angles = Y[1], Y[0]
 y_one_hot, _ = y_to_numbers(Y_classes)
 
-ID_START = 99
+ID_START = 0
 
 hyper_X = list(dl.get_json([path_testsuite]))[0]["testsuiteSlice"]
 
@@ -64,25 +66,23 @@ hyper_X = list(dl.get_json([path_testsuite]))[0]["testsuiteSlice"]
 for i in range(len(hyper_X)):
     pp = Preprocessing(deepcopy(X))
     Xpp = pp.preprocess_data(hyper_X[i])
-    summary_logger = Logger("%s.log" % (i+ID_START))
+    summary_logger = Logger("%s-%s.log" % (model_name,(i+ID_START)))
     
     OOM_interrupt = 0
     t = time.time()
     
-    m_cnn3d = ConvNet3D()
-    model = m_cnn3d.setup_model(Xpp)
-    
+    cmodel = ConvNet3D()
+    model = cmodel.setup_model(Xpp)
     try:
-        # train models multiple times (!GPU)
-        results, summary_string = m_cnn3d.evaluate_model(model, Xpp, y_one_hot)
+        results, summary_string = cmodel.evaluate_model(model, Xpp, y_one_hot)
     except tf.errors.ResourceExhaustedError:
         with tf.device('/cpu:0'):
             t = time.time()
             print("\033[93m[WARNING] Could not use CUDA for testset iteration %s, will switch to CPU instead\033[0m" % i)
             OOM_interrupt = 1
-            results, summary_string = m_cnn3d.evaluate_model(model, Xpp, y_one_hot)
+            results, summary_string = cmodel.evaluate_model(model, Xpp, y_one_hot)
     data_logger.log_data([[(i+ID_START), OOM_interrupt, time.time()-t, *results[0], hyper_X[i], results[1]]])
     summary_logger.log_data([[summary_string]], "summaries")
 
-    del m_cnn3d, pp
+    del cmodel, pp
     gc.collect()
